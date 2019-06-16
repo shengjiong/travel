6c9777197ff145839612f3f812f044cf03964d56
shengqiong <shengqiong007@outlook.com>
2019-06-16 17:08:55 +0800

    首页

--- src/pages/home/components/Icon.vue
+++ src/pages/home/components/Icon.vue
@@ -99,48 +99,44 @@ export default {
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
 @import '~styles/mixins.styl'
-.icons >>> .swiper-container {
+.icons >>> .swiper-container
   overflow: hidden;
   height: 0;
   padding-bottom: 50%;
   // background: green;
-}
+.icons
+  margin-top: .1rem
+  .icon
+    position: relative;
+    overflow: hidden;
+    float: left;
+    width: 25%;
+    height: 0;
+    padding-bottom: 25%;
+    // background: red;
+    .icon-img
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0.44rem;
+      box-sizing: border-box;
+      padding: 0.1rem;
+      // background: blue;
 
-.icon {
-  position: relative;
-  overflow: hidden;
-  float: left;
-  width: 25%;
-  height: 0;
-  padding-bottom: 25%;
-  // background: red;
-  .icon-img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0.44rem;
-    box-sizing: border-box;
-    padding: 0.1rem;
-    // background: blue;
+      .icon-img-content
+        display: block;
+        margin: 0 auto;
+        height: 100%;
 
-    .icon-img-content {
-      display: block;
-      margin: 0 auto;
-      height: 100%;
-    }
-  }
-
-  .icon-desc {
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    height: 0.44rem;
-    line-height: 0.44rem;
-    text-align: center;
-    color: $darkTextColor;
-    ellipsis();
-  }
-}
+    .icon-desc
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      height: 0.44rem;
+      line-height: 0.44rem;
+      text-align: center;
+      color: $darkTextColor;
+      ellipsis();
 </style>
