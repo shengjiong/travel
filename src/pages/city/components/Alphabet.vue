fadb62a9ca8db8dc9c0b095fbb9655b6dbc32284
shengqiong <shengqiong007@outlook.com>
2019-06-17 20:31:41 +0800

    添加节流

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -22,8 +22,13 @@ export default {
   },
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
   },
   computed: {
     letters () {
@@ -31,6 +36,7 @@ export default {
       const letters = []
       for (let i in this.cities) {
         letters.push(i)
+        console.log(letters)
       }
       return letters
     }
@@ -41,15 +47,20 @@ export default {
     },
     handleTouchMove (e) {
       if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop
-        console.log(startY)
-        const touchY = e.touches[0].clientY - 79
-        console.log(touchY)
-        const index = Math.floor((touchY - startY) / 20)
-        console.log(index)
-        if (index >= 0 && index < this.letters.length) {
-          this.$emit('change', this.letters[index])
+        if (this.timer) {
+          clearTimeout(this.timer)
         }
+        this.timer = setTimeout(() => {
+          const startY = this.startY
+          console.log(startY)
+          const touchY = e.touches[0].clientY - 79
+          console.log(touchY)
+          const index = Math.floor((touchY - startY) / 20)
+          console.log(index)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
       }
     },
     handleTouchEnd () {
